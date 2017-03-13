245e598c6265e38f09ba6699dc4c8c21de687b77
mross982 <mross982@gmail.com>
2017-03-12 20:39:30 -0500

    fixed the oeration, but still need to control the order of fields

--- yahooAPI1.py
+++ yahooAPI1.py
@@ -6,47 +6,51 @@ import os
 import datetime
 from dateutil.relativedelta import relativedelta
 from pprint import pprint
+import glob
 
 
 def read_symbols(s_symbols_file):
-    '''Read a list of symbols'''
-  
-    # s_symbols_file = os.path.abspath("Fund_data\\" + s_symbols_file)
+    '''Read each line from a .txt file and return a list of symbols'''
+
+    s_symbols_file = os.path.abspath(s_symbols_file)
     ls_symbols = []
     ffile = open(s_symbols_file, 'r')
     for line in ffile.readlines():
         str_line = str(line)
         if str_line.strip(): 
-            ls_symbols.append(str_line.strip())
+        	ls_symbols.append(str_line.strip())
     ffile.close()
+  
     return ls_symbols 
+
 
 def daily_update(ls_symbols, start_date, end_date):
 
 	updates = []
-	for sec in ls_symbols:
+	for symbol in ls_symbols:
+		print symbol
 		try:
-			print 'Getting ' + sec + ' data from Yahoo...'
-			security = Share(sec)
+			security = Share(symbol)
+			print 'getting data from Yahoo...'
 			tick = [
 			security.get_historical(str(start_date), str(end_date))
 			]
-			updates.append(tick)
+			for data in tick:
+				updates.append(data)
 		except:
-			print 'No data for ' + tick
-			pass
+			print 'Could not find data for ' + tick
 
 	writedata(updates)
 
 
 def writedata(updates):
 
-	print 'csv files created'
 	for items in updates:
 		for thing in items:
 			keys = thing[0].keys()
-			ticker_symbol = thing[0]
-			ticker_symbol = str(ticker_symbol['Symbol'])
+			print keys
+			sys.exit(0)
+			ticker_symbol = thing[0]['Symbol']
 			path = 'C:\Python27\Lib\site-packages\QSTK\QSData\Yahoo'
 		
 			with open(path + '\\' + ticker_symbol + '.csv', 'wb') as csv_file:
@@ -75,14 +79,17 @@ if __name__ == '__main__':
 	start_date = end_date - relativedelta(years=1)
 
 	script_dir = os.path.dirname(__file__)
-	rel_path = "accounts\\"
+	rel_path1 = "accounts\\*"
+	rel_path2 = "accounts\\" 
+	
+	# print os.path.realpath(__file__) finds this script's path
 
 	if len(sys.argv) == 1:
-		for file in glob.glob(os.path.join(script_dir, rel_path, '*')):
-			print filename
+		for file in glob.glob(os.path.join(script_dir, rel_path1)):
+			filename = file
 			ls_symbols = read_symbols(filename)
 			daily_update(ls_symbols, start_date, end_date)
 	else: 
 		filename = sys.argv[1]
-		ls_symbols = read_symbols(os.path.join(script_dir, rel_path, filename))
+		ls_symbols = read_symbols(os.path.join(script_dir, rel_path2, filename))
 		daily_update(ls_symbols, start_date, end_date)
